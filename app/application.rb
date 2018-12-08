5bc8f690b8f2ae6cef69eacf6f2a423892e47f97
susan <bluenote@outlook.com>
2018-12-08 15:50:32 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,13 +7,13 @@ class Application
   	req = Rack::Request.new(env) # notice Request not response here.
   	#parses the user request contained in env
 
-    # if req.path == "/items"
+    if req.path != "/items"
     # 	resp.write "You requested the items"
     # else
     #   resp.write 	"Route not found"
-    #   resp.status = 404
-    #  end
-    if req.path.match(/items/)
+       resp.status = 404
+      end
+     req.path.match(/items/)
     	item_to_find = req.path.split("/items/").last
     	  if item = @@items.find do |item|
           item.name == item_to_find
@@ -23,10 +23,10 @@ class Application
           resp.status = 400
           resp.write "Item not found"
         end
-     else
-     	 resp.status = 404
-       resp.write 	"Route not found"
-      end
+     # else
+     # 	 resp.status = 404
+     #   resp.write 	"Route not found"
+     #  end
      resp.finish
   end
 end
